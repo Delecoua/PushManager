32bfde9890458821df3bd709af3575c64136698a
Delecoua <aymeric1829@gmail.com>
2018-04-28 15:44:23 +0200

    Add the first controller
    Add the controller, with first basic method
    Add a service in the container to allow the app to use mvc

---
+++ PushManager/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PushManager.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Home")]
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public JsonResult Home()
+        {
+            return new JsonResult("Hello World");
+        }
+    }
+}
